359634bdab415fd254a5b37e87c2035425a02c98
mikegent01 <70764301+mikegent01@users.noreply.github.com>
2025-12-02 11:59:06 -0500

    Update global-war.js

--- Reputation-Matrix2/global-war.js
+++ Reputation-Matrix2/global-war.js
@@ -3,7 +3,7 @@
 import { getAllFactions, getFaction, getFactionStats, toSystemId } from './systems/faction-registry.js';
 import { getRealTimeMapStats, getCuratedTerritoryList, getDetailedRegionStats, renderAnalyticsModal, getDetailedFactionStats } from './global-map-analysis.js';
 import { CURRENT_GAME_DATE } from './calendar-data.js';
-import { MAP_DATA } from '../Reputation-Matrix2/map-data.js';
+import { MAP_DATA } from './map-data.js';
 import { renderTerritoryDetailModal } from './systems/mushroom-kingdom-system.js'; 
 // ============================================
 // STATE
