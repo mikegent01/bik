09bd99fb0fa495267f3b782bcbcb694920bf30df
mikegent01 <70764301+mikegent01@users.noreply.github.com>
2026-08-05 22:17:20 +0000

    Organize regional battle map data
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- Reputation-Matrix2/data/maps/map-battle-data.js
+++ Reputation-Matrix2/data/maps/map-battle-data.js
@@ -1,14 +1,14 @@
 // This file aggregates all battle data from modular files to provide a complete tactical overview.
 
-import { BASE_BATTLE_DATA } from './map-battle-data-base.js';
-import { MUSHROOM_KINGDOM_TROOPS } from './map-battle-data-mk.js';
-import { MIDLANDS_TROOPS } from './map-battle-data-midlands.js';
-import { WIDESPREAD_TROOPS } from './map-battle-data-widespread.js';
-import { MIDDLE_EARTH_TROOPS } from './map-battle-data-middle-earth.js';
-import { INTERNET_TROOPS } from './map-battle-data-internet.js';
-import { WARHAMMER_TROOPS } from './map-battle-data-warhammer.js';
-import { KIVOTOS_TROOPS } from './map-battle-data-kivotos.js';
-import { POKEMON_TROOPS } from './map-battle-data-pokemon.js';
+import { BASE_BATTLE_DATA } from './battles/map-battle-data-base.js';
+import { MUSHROOM_KINGDOM_TROOPS } from './battles/map-battle-data-mk.js';
+import { MIDLANDS_TROOPS } from './battles/map-battle-data-midlands.js';
+import { WIDESPREAD_TROOPS } from './battles/map-battle-data-widespread.js';
+import { MIDDLE_EARTH_TROOPS } from './battles/map-battle-data-middle-earth.js';
+import { INTERNET_TROOPS } from './battles/map-battle-data-internet.js';
+import { WARHAMMER_TROOPS } from './battles/map-battle-data-warhammer.js';
+import { KIVOTOS_TROOPS } from './battles/map-battle-data-kivotos.js';
+import { POKEMON_TROOPS } from './battles/map-battle-data-pokemon.js';
 
 export const BATTLE_MAP_DATA = {
     // Front lines and the Vigilance journey are defined in the base file.
